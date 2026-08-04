2757b0108c13c9aaad14e472964ac9f4db93d3c7
nanabytz <nandhagopan.s007@gmail.com>
2026-08-04 12:06:56 +0530

    Learned JS operators

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-//ooty apple
-var fruit="Apple"
-console.log(fruit)
-/* This is a very tasty apple */
+var a=10
+a++
+
+var b=20
+b--
+
+var c=30
+c=++c
+var d=40
+d=--d
+console.log(c,d)
+console.log(a)
+console.log(b)
